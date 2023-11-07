7747b36fc91d1f769b7c6d5b2be1af430051ee39
Tanay Upadhyaya <tanayupadhyaya@gmail.com>
2023-11-07 22:15:52 +0530

    Response error handling for v1_send_session_message_whatsapp_number_post

--- lib/wati_client/models/response.rb
+++ lib/wati_client/models/response.rb
@@ -1,9 +1,10 @@
 module WatiClient
   class Response
-    def initialize(data, status_code, headers)
+    def initialize(data, status_code, headers, error)
       @data = data
       @status_code = status_code
       @headers = headers
+      @error = error
     end
 
     def data
@@ -16,6 +17,10 @@ module WatiClient
 
     def headers
       @headers
+    end
+
+    def error
+      @error
     end
   end
 end
